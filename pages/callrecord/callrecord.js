b5593706fa037535ace8ab5d7323b2f3e88ab1de
meng.yang_wh <meng.yang_wh@unistrong.com>
2023-02-17 22:11:28 +0800

    通话记录播放视频

--- pages/callrecord/callrecord.js
+++ pages/callrecord/callrecord.js
@@ -6,6 +6,10 @@ const utils = require('../../utils/util')
 const App = getApp();
 //声明一个公共的API接口
 var audioCtx = wx.createInnerAudioContext();
+let attrType = {
+    voice: 1,//音频
+    video: 2,//视频
+}
 Page({
 
     /**
@@ -26,14 +30,14 @@ Page({
         this.setData({
             loginName: wx.getStorageSync('userInfo').loginName
         })
-        audioCtx.onEnded( ()=> {
+        audioCtx.onEnded(() => {
             console.log("监听播放结束")
             //监听音频自然播放至结束的事件
             this.setData({
                 isPlay: false,
                 isPause: false
             })
-         })
+        })
     },
 
     getCallRocords() {
@@ -48,6 +52,7 @@ Page({
                 for (let i = 0; i < res.data.rows.length; i++) {
                     res.data.rows[i].times = utils.humandate(res.data.rows[i].billsec);
                     res.data.rows[i].isTouchMove = false;
+                    res.data.rows[i].callType = res.data.rows[i].recordname.includes(".wav") ? attrType.voice : attrType.video;
                     res.data.rows[i].recordname = constants.attrUrl + '/' + res.data.rows[i].recordname;
                 }
                 this.setData({
@@ -125,15 +130,15 @@ Page({
     },
     //页面关闭时，停止播放
     onHide() {
-        console.log("uuuuonHide===>",this.data.isPlay)
-        if(this.data.isPlay){
+        console.log("uuuuonHide===>", this.data.isPlay)
+        if (this.data.isPlay) {
             this.audioPause();
-        }    
+        }
     },
     onUnload() {
-        if(this.data.isPlay){
+        if (this.data.isPlay) {
             this.audioPause();
-        }  
+        }
     },
     //设置音乐栏目默认值
     setMusic: function (index) {
@@ -145,19 +150,32 @@ Page({
     },
 
     itemClick(e) {
-        if(this.data.callRecordList[e.currentTarget.dataset.index].billsec <= 0){
+        let type = this.data.callRecordList[e.currentTarget.dataset.index].callType;
+        if (this.data.callRecordList[e.currentTarget.dataset.index].billsec <= 0) {
             ui.showToast("无通话时长!");
             return;
         }
-        if(this.data.playIndex == e.currentTarget.dataset.index && this.data.isPlay){
-            console.log("暂停")
-            this.audioPause();
-            this.setData({
-                isPause: true
+        if (type == attrType.voice) {
+            if (this.data.playIndex == e.currentTarget.dataset.index && this.data.isPlay) {
+                this.audioPause();
+                this.setData({
+                    isPause: true
+                })
+                return;
+            }
+            this.setMusic(e.currentTarget.dataset.index)
+            this.audioPlay();
+        } else {
+            if (this.data.isPlay) {
+                this.audioPause();
+                this.setData({
+                    isPause: true,
+                    isPlay: false
+                })
+            }
+            wx.navigateTo({
+                url: `/pages/callrecord/callrecordvedio/callrecordvedio?src=${this.data.callRecordList[e.currentTarget.dataset.index].recordname}&title=${this.data.callRecordList[e.currentTarget.dataset.index].callednum}`,
             })
-            return;
         }
-        this.setMusic(e.currentTarget.dataset.index)
-        this.audioPlay();
     },
 })
---
+++ pages/callrecord/callrecordvedio/callrecordvedio.js
@@ -0,0 +1,29 @@
+Page({
+
+    onLoad(options) {
+        console.log("options:", options);
+        this.setData({
+            src: options.src,
+        })
+        wx.setNavigationBarTitle({
+            title: options.title,
+        })
+    },
+    onReady() {
+        this.videoContext = wx.createVideoContext('myVideo');
+        this.videoContext.play()
+    },
+    data: {
+        src: '',
+    },
+
+    bindPlayVideo() {
+        console.log('1')
+        this.videoContext.play()
+    },
+
+    videoErrorCallback(e) {
+        console.log('视频错误信息:')
+        console.log(e.detail.errMsg)
+    }
+})
